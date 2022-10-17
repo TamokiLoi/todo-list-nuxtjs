5928c576a96e8d1cf2a841b3949d65fa4bbca2c6
Nguyen Lam Thanh Loi <loinguyenlamthanh@gmail.com>
2022-10-17 14:41:19 +0700

    init src

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <main>
+    <h1>Task Board</h1>
+    <p>Create a list of tasks</p>
+    <div class="create-new">
+      <input
+        type="text"
+        v-model="newTask"
+        placeholder="Add a new task"
+        @keypress.enter="addTask"
+      />
+      <button @click="addTask">Add Task</button>
+    </div>
+
+    <div class="tasks">
+      <!-- Component -->
+      <Task
+        v-for="(task, index) in $store.state.tasks"
+        :key="index"
+        :task="task"
+      />
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      newTask: "",
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask) {
+        console.log("this.newTask", this.newTask);
+        this.$store.commit("ADD_TASK", this.newTask);
+        this.newTask = "";
+      }
+    },
+  },
+};
+</script>
